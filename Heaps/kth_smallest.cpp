d1d82afa6dfe77c6663d966aa8e017bab8447d69
skm2000 <shubhamskm2000@gmail.com>
2020-06-30 16:25:30 +0530

    kth smallest

--- Heaps/kth_smallest.cpp
+++ Heaps/kth_smallest.cpp
@@ -1,7 +1,7 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-// use a max heap
+// use a max heap (smallest=>max-heap)
 int findKthSmallest(vector<int>&nums, int k,int n) {
     priority_queue<int>max_heap;
     for(int i=0;i<n;i++){
